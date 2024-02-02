ace4c9a3dcd4e0f9d14723d359f760d3b75fe7f1
Aryan <aupathak1234@gmail.com>
2024-02-02 13:18:38 +0530

    Initial commit

---
+++ day_1/taking_input.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+void main(){
+    int a,b, result;
+    printf("Enter two values:");
+    scanf("%d%d", &a, &b);
+    result = a+b;
+    printf("sum is equal to %d", result);
+}
---
+++ day_2/control structure.c
@@ -0,0 +1,14 @@
+// Control structures
+// 1. Conditional statements: if, else
+// 2. Iterative: for, while
+
+
+#include<stdio.h>
+
+void main(){
+    int i;
+
+    for(i=1; i<=5; i++){
+        printf("Computer %d \n", i);
+    }
+}
